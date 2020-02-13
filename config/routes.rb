5f17dc56d37d1cd86b68ff0e895acf3b0da84ef6
ryushokawamura <60692960+ryushokawamura@users.noreply.github.com>
2020-02-13 19:24:38 +0900

    change

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   devise_for :users
   root "messages#index"
   resources :users, only: [:edit, :update]
-  resources :groups, only: [:new, :create]
+  resources :groups, only: [:index, :new, :create, :edit, :update]
 end
